f664f8821df9d3d96dd3e462d38150a0d2e42cf2
edumarques687 <ermm.rs@gmail.com>
2021-03-22 15:30:37 -0300

    Settings for Static and Media for server

--- Grimorio_T20/settings.py
+++ Grimorio_T20/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '*507zv*13%rjj9+itn#5_fx9e!ojb0soly9)o@j!ig10!xp0#v'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['eduardomarques.pythonanywhere.com']
 
 
 # Application definition
@@ -122,8 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR / 'static')
 
 MEDIA_URL = '/media/'
-
-MEDIA_ROOT = BASE_DIR / 'media'
 MEDIA_ROOT = os.path.join(BASE_DIR / 'media')
