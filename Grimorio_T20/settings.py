b05b0f5ad41341e43f3ef83b5dc9bd8d33e119c5
MeurerMarques <Eduardo_Marques1@Dell.com>
2022-06-13 12:34:31 -0300

    Adding HB spell images

--- Grimorio_T20/settings.py
+++ Grimorio_T20/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '*507zv*13%rjj9+itn#5_fx9e!ojb0soly9)o@j!ig10!xp0#v'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
@@ -124,10 +124,10 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 # Deploy config
-# STATIC_ROOT = os.path.join(BASE_DIR / 'static')
+STATIC_ROOT = os.path.join(BASE_DIR / 'static')
 
 # Local config
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static/')]  # add STATIC_ROOT to DIRS
+# STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static/')]  # add STATIC_ROOT to DIRS
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR / 'media/')
